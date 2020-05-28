7f2dec52e5d4baa6dfc200aee58d81f0ea6ef7e0
Mohammed Irfan <mohammedirf1990@gmail.com>
2020-05-28 09:27:11 +0400

    add config variables in heroku

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.static("public"));
 app.set('view-engine', 'ejs');
 
 app.use(session({
-  secret: "thisismylittlesecret",
+  secret: process.env.DB_SECRET,
   resave: false,
   saveUninitialized: false
 }));
